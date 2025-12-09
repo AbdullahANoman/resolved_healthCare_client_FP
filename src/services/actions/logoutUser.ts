6dcef110d89b06bcf8883964e55ef6676456c909
Abdullah Al Noman <abdullahalnoman@Abdullahs-Mac-mini.local>
2025-12-09 09:21:02 +0600

    update at cache clean

--- src/services/actions/logoutUser.ts
+++ src/services/actions/logoutUser.ts
@@ -5,6 +5,7 @@ import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.share
 export const logoutUser = (router: AppRouterInstance) => {
   localStorage.removeItem(authKey);
   deleteCookies([authKey, "refreshToken", "accessToken"]);
+  deleteCookies(["accessToken"]);
   router.push("/");
   setTimeout(() => {
     window.location.href = "/login";
